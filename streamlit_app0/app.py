70dd390cc218348ca42fcf849b96d02f8ea042ff
mustafaksr <95983597+mustafaksr@users.noreply.github.com>
2024-07-17 11:28:06 +0300

    remove mysql

--- streamlit_app0/app.py
+++ streamlit_app0/app.py
@@ -6,18 +6,18 @@ st.write("""## Hello World """)
 st.write("""### Hello World""")
 
 
-# Initialize connection.
-conn = st.connection('mysql', type='sql')
+# # Initialize connection.
+# conn = st.connection('mysql', type='sql')
 
-# Perform query.
-df = conn.query('SELECT * from wp_users;', ttl=600)
+# # Perform query.
+# df = conn.query('SELECT * from wp_users;', ttl=600)
 
-# Print results.
-for row in df.itertuples():
-    st.write(f"user_login : {row.user_login} |  user_pass : {row.user_pass}:")
+# # Print results.
+# for row in df.itertuples():
+#     st.write(f"user_login : {row.user_login} |  user_pass : {row.user_pass}:")
 
-st.write("# df")
-st.dataframe(df)
+# st.write("# df")
+# st.dataframe(df)
 
-st.write("# df columns")
-st.dataframe(df[["user_login","user_pass"]])
+# st.write("# df columns")
+# st.dataframe(df[["user_login","user_pass"]])
