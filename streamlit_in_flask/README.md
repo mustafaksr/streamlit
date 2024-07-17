9487ad02c2b6c3f9473014bcf45ba6da50bdd86a
mustafaksr <95983597+mustafaksr@users.noreply.github.com>
2024-07-17 18:10:21 +0300

    streamlit in flask

---
+++ streamlit_in_flask/README.md
@@ -0,0 +1,5 @@
+```
+docker build -t flask_streamlit_app .
+
+docker run -p 5000:5000 -p 8501:8501 flask_streamlit_app
+```
---
+++ streamlit_in_flask/streamlit_app.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title("Welcome to the Streamlit app!")
+st.write("This is a simple Streamlit application.")
